7fbc17453ec1c1dba92b4f3a5e3ee00dc8fe1e09
L.I.A. Jonathan Bedolla Hurtado <=>
2021-05-31 17:40:26 -0500

    Se sigue mejorando Jetstream

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <!--<x-jet-welcome />-->
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam quidem excepturi facere ad, veniam sunt alias suscipit commodi? Atque sapiente natus accusantium corrupti molestiae ab sint commodi neque delectus provident? 
+                <x-registrarLibroComponent />
             </div>
         </div>
     </div>
