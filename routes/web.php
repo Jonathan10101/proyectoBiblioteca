bf5d77902ea474b2dc25f98e440252057c6d46be
L.I.A. Jonathan Bedolla Hurtado <=>
2021-05-31 15:16:54 -0500

    Se agrega jetstream

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view("login");
+    return view("welcome");
 });
 
 Route::get('libros/crear',[LibroController::class,"index"]);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
